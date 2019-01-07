d2a621acb6e6585fee4e3165660ab324eb7df09c
epost <25180070+erpost@users.noreply.github.com>
2019-01-07 15:26:53 -0500

    commit

--- bigquery/get_table_columns_distinct.sh
+++ bigquery/get_table_columns_distinct.sh
@@ -15,14 +15,13 @@ bq query --use_legacy_sql=false --format=json "SELECT table_id FROM \`$dataset._
 
 for table in $(cat /tmp/tables);
 do
-        for column in $(bq show --format=json $dataset.$table | jq -r '.schema.fields[].name')
-        do
-                for data in $(bq query --use_legacy_sql=false --format=json "select distinct $column from $dataset.$table" | jq -r .[].$column)
-                do
-                        echo "\"$table\",\"$column\",\"$data\"" | tee -a tables_columns_distinct.csv
-
-                done
-        done
+    for column in $(bq show --format=json $dataset.$table | jq -r '.schema.fields[].name')
+    do
+            for data in $(bq query --use_legacy_sql=false --format=json "select distinct $column from $dataset.$table" | jq -r .[].$column)
+            do
+                    echo "\"$table\",\"$column\",\"$data\"" | tee -a tables_columns_distinct.csv
+            done
+    done
 done
 
 rm -f /tmp/tables
